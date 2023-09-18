99480d8c1dbb6827b69257231249b1bd97049ab3
confusinguser <25439218+confusinguser@users.noreply.github.com>
2023-09-18 21:19:20 +0200

    Remove grace period when triggering swaylock on startup

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -12,11 +12,11 @@ case $(( $RANDOM % 4 )) in
     ;;
 esac
 
-args="--clock --indicator --effect-blur 5x10 --fade-in 0.2 --ring-color ${colors[0]} --effect-vignette 0.5:0.5 --grace 2 --inside-color ${colors[1]} --text-color ${colors[2]} "
+args="--clock --indicator --effect-blur 5x10 --fade-in 0.2 --ring-color ${colors[0]} --effect-vignette 0.5:0.5 --inside-color ${colors[1]} --text-color ${colors[2]} "
 
 if [ $# -eq 0 ] 
 then
-  swaylock ${args} --screenshots --effect-scale 1.1 --scaling center
+  swaylock ${args} --screenshots --effect-scale 1.1 --scaling center --grace 2
 else
   swaylock ${args} $@
 fi
