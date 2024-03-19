baee8dd36dfc1ced9030f04f2242bdac842c42c8
confusinguser <25439218+confusinguser@users.noreply.github.com>
2024-03-19 13:55:43 +0100

    Change args in lock.sh

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -6,7 +6,7 @@ case $(( $RANDOM % 4 )) in
     ;;
   1) colors=("8C2F39" "461220CC" "FCB8B0") # Hot incinerator
     ;;
-  2) colors=("7A308C" "321245DD" "EE81EE") # Bottom of the rainbow
+  2) colors=("7A308C" "321245DD" "EE81EE") # Purple
     ;;
   3) colors=("18A300" "102B04CC" "1983E0") # Cactus flower
     ;;
@@ -14,9 +14,9 @@ case $(( $RANDOM % 4 )) in
   #   ;;
 esac
 
-args="--clock --indicator --effect-blur 5x10 --fade-in 0.2 --ring-color ${colors[0]} --effect-vignette 0.5:0.5 --inside-color ${colors[1]} --text-color ${colors[2]} "
+args="--clock --indicator --effect-blur 3x5 --fade-in 0.2 --ring-color ${colors[0]} --effect-vignette 0.5:0.5 --inside-color ${colors[1]} --text-color ${colors[2]} --ignore-empty-password --datestr %A %-d %b"
 
-if [ $# == 0 ] || [ $0 != "--image" ]
+if [ $# == 0 ] || [ $1 != "--image" ]
 then
   swaylock ${args} --screenshots --effect-scale 1.1 --scaling center $@
 else
